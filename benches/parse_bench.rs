c9eb161838158b4f1f07e88e220788203287a804
Alex Choi <k5j9b7c6fb@privaterelay.appleid.com>
2026-01-02 09:58:02 -0500

    Add benchmarks for expander and printer
    - expand simple syntax-quote: 1.20 µs
    - expand complex syntax-quote: 2.30 µs
    - expand macro-style: 2.54 µs
    - print simple forms: 287 ns
    - print complex forms: 871 ns
    - expand + print pipeline: 3.29 µs

--- benches/parse_bench.rs
+++ benches/parse_bench.rs
@@ -1,5 +1,5 @@
 use bumpalo::Bump;
-use clojure_parser::parse;
+use clojure_parser::{parse, Expander, print_forms};
 use criterion::{Criterion, black_box, criterion_group, criterion_main};
 
 const SIMPLE_FORMS: &str = r#"
@@ -50,6 +50,29 @@ const NESTED_STRUCTURE: &str = r#"
 {:level1 {:level2 {:level3 {:level4 {:level5 {:data [1 2 3 4 5]}}}}}}
 [[[[[1 2] [3 4]] [[5 6] [7 8]]] [[[9 10] [11 12]] [[13 14] [15 16]]]]]
 (((((println "deeply nested")))))
+"#;
+
+const SYNTAX_QUOTE_SIMPLE: &str = r#"
+`foo
+`(a b c)
+`[1 2 3]
+`{:a 1 :b 2}
+"#;
+
+const SYNTAX_QUOTE_COMPLEX: &str = r#"
+`(defn ~name [~@args]
+  (let [x# (first ~@args)
+        y# (second ~@args)]
+    {:result (+ x# y#)
+     :inputs [~@args]}))
+"#;
+
+const SYNTAX_QUOTE_MACRO: &str = r#"
+`(let [start# (System/nanoTime)
+       result# (do ~@body)
+       elapsed# (- (System/nanoTime) start#)]
+   (println ~name "took" (/ elapsed# 1e6) "ms")
+   result#)
 "#;
 
 fn bench_simple_forms(c: &mut Criterion) {
@@ -105,12 +128,95 @@ fn bench_with_reused_bump(c: &mut Criterion) {
     });
 }
 
+fn bench_expand_simple(c: &mut Criterion) {
+    c.bench_function("expand simple syntax-quote", |b| {
+        b.iter(|| {
+            let bump = Bump::new();
+            let forms = parse(black_box(SYNTAX_QUOTE_SIMPLE), &bump).unwrap();
+            let mut expander = Expander::new(&bump);
+            for f in forms.iter() {
+                black_box(expander.expand(&f.value).unwrap());
+            }
+        })
+    });
+}
+
+fn bench_expand_complex(c: &mut Criterion) {
+    c.bench_function("expand complex syntax-quote", |b| {
+        b.iter(|| {
+            let bump = Bump::new();
+            let forms = parse(black_box(SYNTAX_QUOTE_COMPLEX), &bump).unwrap();
+            let mut expander = Expander::new(&bump);
+            for f in forms.iter() {
+                black_box(expander.expand(&f.value).unwrap());
+            }
+        })
+    });
+}
+
+fn bench_expand_macro(c: &mut Criterion) {
+    c.bench_function("expand macro-style syntax-quote", |b| {
+        b.iter(|| {
+            let bump = Bump::new();
+            let forms = parse(black_box(SYNTAX_QUOTE_MACRO), &bump).unwrap();
+            let mut expander = Expander::new(&bump);
+            for f in forms.iter() {
+                black_box(expander.expand(&f.value).unwrap());
+            }
+        })
+    });
+}
+
+fn bench_print_simple(c: &mut Criterion) {
+    let bump = Bump::new();
+    let forms = parse(SIMPLE_FORMS, &bump).unwrap();
+    let form_refs: Vec<_> = forms.iter().map(|f| &f.value).collect();
+
+    c.bench_function("print simple forms", |b| {
+        b.iter(|| {
+            black_box(print_forms(form_refs.iter().copied()))
+        })
+    });
+}
+
+fn bench_print_complex(c: &mut Criterion) {
+    let bump = Bump::new();
+    let forms = parse(COMPLEX_FORMS, &bump).unwrap();
+    let form_refs: Vec<_> = forms.iter().map(|f| &f.value).collect();
+
+    c.bench_function("print complex forms", |b| {
+        b.iter(|| {
+            black_box(print_forms(form_refs.iter().copied()))
+        })
+    });
+}
+
+fn bench_expand_and_print(c: &mut Criterion) {
+    c.bench_function("expand + print macro", |b| {
+        b.iter(|| {
+            let bump = Bump::new();
+            let forms = parse(black_box(SYNTAX_QUOTE_MACRO), &bump).unwrap();
+            let mut expander = Expander::new(&bump);
+            let expanded: Vec<_> = forms.iter()
+                .map(|f| expander.expand(&f.value).unwrap())
+                .collect();
+            black_box(print_forms(expanded.iter()))
+        })
+    });
+}
+
 criterion_group!(
     benches,
     bench_simple_forms,
     bench_complex_forms,
     bench_nested_structure,
     bench_many_tokens,
-    bench_with_reused_bump
+    bench_with_reused_bump,
+    bench_expand_simple,
+    bench_expand_complex,
+    bench_expand_macro,
+    bench_print_simple,
+    bench_print_complex,
+    bench_expand_and_print
 );
 criterion_main!(benches);
